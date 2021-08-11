4d52341718094c2e3831b08d633fab4d791a53bc
DenDenDor <dendor333@gmail.com>
2021-08-12 02:57:28 +0300

    add

---
+++ test1/Assets/Enemy/IEnemy.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+public interface IEnemy 
+{
+  void SetPlayer(Player _character);
+    int Speed(int _speed);
+   int Health(int _health);
+   int Damage(int _damage);
+   void Attack();
+   void Move();
+}
---
+++ test1/Assets/Enemy/Skeleton.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour, IEnemy
+{
+    [SerializeField] private Player _player;
+
+    private void Update() {
+       Move();
+   }
+
+    public void Attack()
+    {
+        Debug.Log("Attack");
+        Damage(3);
+        Health(0);
+    }
+
+    public int Damage(int _damage)
+    {
+        return _damage + 1;
+    }
+
+    public int Health(int _health)
+    {
+       return _health + 2;
+    }
+
+    public void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, _player.transform.position, Speed(4));
+        if (Vector2.Distance(_player.transform.position, transform.position) < 0.5f)
+        {
+            Attack();
+        }
+    }
+
+    public int Speed(int _speed)
+    {
+        return _speed + 4;
+    }
+
+    public void SetPlayer(Player _character)
+    {
+        _player =_character;
+    }
+}
