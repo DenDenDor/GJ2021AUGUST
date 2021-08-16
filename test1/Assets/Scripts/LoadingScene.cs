bcb3ba8abd51457e5c5ea0481ebbcc75ec356b96
DenDenDor <dendor333@gmail.com>
2021-08-16 13:45:28 +0300

    add

--- test1/Assets/Scripts/LoadingScene.cs
+++ test1/Assets/Scripts/LoadingScene.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 public class LoadingScene : MonoBehaviour
 {
-
-[SerializeField] private int _number_of_lastScene;
-[SerializeField] private int _number_of_firstScene;
- private void LoadScene(int _numberOfScene) => SceneManager.LoadScene(_numberOfScene);
- 
- public void Exit() => LoadScene(_number_of_lastScene);
- public void Play() => LoadScene(_number_of_firstScene);
-
+[SerializeField] private int _waitTime;
+[SerializeField] private int _number_current_scene;
+[SerializeField] private bool _isIntermediateScene;
+ private void Start() 
+ {
+     if(_isIntermediateScene)
+     {
+     StartCoroutine(Load());
+     }
+ }
+ public void LoadScene(int _numberOfScene) => SceneManager.LoadScene(_numberOfScene);
+ private IEnumerator Load()
+ {
+     yield return new WaitForSeconds(_waitTime);
+     LoadScene(_number_current_scene);
+ }
  
 
 }
