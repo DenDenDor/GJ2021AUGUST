947d910a13b59a2754545f1ae1458221a7536a69
DenDenDor <dendor333@gmail.com>
2021-08-17 11:38:42 +0300

    add

--- test1/Assets/Scripts/LoadingScene.cs
+++ test1/Assets/Scripts/LoadingScene.cs
@@ -1,23 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
 public class LoadingScene : MonoBehaviour
 {
 [SerializeField] private int _waitTime;
 [SerializeField] private int _number_current_scene;
 [SerializeField] private bool _isIntermediateScene;
+    private int _sceneNumber;
+	[SerializeField] private Slider slider;
+	[SerializeField] private GameObject _panel;
+
+	public void Initialize(int number) => _sceneNumber = number;
+	
+    public void LoadScene() => StartCoroutine(Load());
+    
+	public void Exit() => Application.Quit();
+
+
  private void Start() 
  {
-     if(_isIntermediateScene)
-     {
-     StartCoroutine(Load());
-     }
+     _panel?.SetActive(false);
  }
- public void LoadScene(int _numberOfScene) => SceneManager.LoadScene(_numberOfScene);
  private IEnumerator Load()
  {
-     yield return new WaitForSeconds(_waitTime);
-     LoadScene(_number_current_scene);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(_sceneNumber);
+
+		_panel?.SetActive(true);
+
+
+		while (!asyncLoad.isDone) 
+		{
+			slider.value = Mathf.Lerp(slider.value,(asyncLoad.progress / .9f), 0.1f);
+			yield return null;
+		}
+	
  }
  
 
